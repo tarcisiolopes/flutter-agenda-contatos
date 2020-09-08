2a684b812b5d3084f7674e9c66c3e43b2e77a9e5
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-08 10:50:23 -0300

    Criando Botão Flutuante

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -10,24 +10,39 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contact> contacts = List();
+
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-
-    Contact c = Contact();
-    c.nome = "Tarcisio";
-    c.sobrenome = "Lopes";
-    c.email = "tarcisiolopesgomes@gmail.com";
-    c.telefone = "062999158371";
-    c.img = "imgtest";
-
-    helper.saveContact(c);
     helper.getAllContacts().then((list){
-      print(list);
+      setState(() {
+        contacts = list;
+      });
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.blue,centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index){
+
+          }
+      ),
+    );
   }
 }
