f973cdaf3289fb3fe7c80c3027a5e535f94e0476
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-08 09:48:33 -0300

    Inicializando o Banco de Dados

--- lib/helper/contact_helper.dart
+++ lib/helper/contact_helper.dart
@@ -1,5 +1,7 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nomeColumn = "nomeColumn";
 final String sobrenomeColumn = "sobrenomeColumn";
@@ -7,7 +9,35 @@ final String emailColumn = "emailColumn";
 final String telefoneColumn = "telefoneColumn";
 final String imgColumn = "imgColumn";
 
-class ContactHelper {}
+class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
+
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  Future<Database> initDb() async {
+    final databasePath = await getDatabasesPath();
+    final path = join(databasePath, "contacts.db");
+
+    return openDatabase(path, version: 1,
+        onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+          "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nomeColumn TEXT, $sobrenomeColumn TEXT, $emailColumn TEXT, $telefoneColumn TEXT, $imgColumn TEXT)");
+    });
+  }
+}
 
 class Contact {
   int id;
