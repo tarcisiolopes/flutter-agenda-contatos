0254228372e0ca2375f1ae21abb0255e72f09ec5
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-08 12:18:37 -0300

    Layout Tela de Contato

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -1,8 +1,10 @@
+import 'dart:io';
+
 import 'package:agenda/helper/contact_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatefulWidget {
-
   final Contact contact;
 
   ContactPage({this.contact});
@@ -12,6 +14,12 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+  final _nomeController = TextEditingController();
+  final _sobrenomeController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _telefoneController = TextEditingController();
+
+  bool _userEdited = false;
 
   Contact _editedContact;
 
@@ -19,10 +27,15 @@ class _ContactPageState extends State<ContactPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    if(widget.contact == null){
+    if (widget.contact == null) {
       _editedContact = Contact();
     } else {
       _editedContact = Contact.fromMap(widget.contact.toMap());
+
+      _nomeController.text = _editedContact.nome;
+      _sobrenomeController.text = _editedContact.sobrenome;
+      _emailController.text = _editedContact.email;
+      _telefoneController.text = _editedContact.telefone;
     }
   }
 
@@ -35,9 +48,65 @@ class _ContactPageState extends State<ContactPage> {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.save),
         backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              child: Container(
+                width: 140.0,
+                height: 140.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null
+                          ? FileImage(File(_editedContact.img))
+                          : AssetImage("images/person.png")),
+                ),
+              ),
+            ),
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text) {
+                _userEdited = true;
+                setState(() {
+                  _editedContact.nome = text;
+                });
+              },
+            ),
+            TextField(
+              controller: _sobrenomeController,
+              decoration: InputDecoration(labelText: "Sobrenome"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.sobrenome = text;
+              },
+            ),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(labelText: "E-mail"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              controller: _telefoneController,
+              decoration: InputDecoration(labelText: "Telefone"),
+              onChanged: (text) {
+                _userEdited = true;
+                _editedContact.telefone = text;
+              },
+              keyboardType: TextInputType.phone,
+            )
+          ],
+        ),
       ),
     );
   }
