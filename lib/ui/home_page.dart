b4aaee1ed0d53873da8357b4162c72d05ea05e24
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-08 11:15:57 -0300

    Cartões de Contatos

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda/helper/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -7,7 +9,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   ContactHelper helper = ContactHelper();
 
   List<Contact> contacts = List();
@@ -16,7 +17,7 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    helper.getAllContacts().then((list){
+    helper.getAllContacts().then((list) {
       setState(() {
         contacts = list;
       });
@@ -28,20 +29,71 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Contatos"),
-        backgroundColor: Colors.blue,centerTitle: true,
+        backgroundColor: Colors.blue,
+        centerTitle: true,
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: () {},
         child: Icon(Icons.add),
         backgroundColor: Colors.blue,
       ),
       body: ListView.builder(
           padding: EdgeInsets.all(10.0),
           itemCount: contacts.length,
-          itemBuilder: (context, index){
+          itemBuilder: (context, index) {
+            return _contactCard(context, index);
+          }),
+    );
+  }
 
-          }
+  Widget _contactCard(BuildContext context, int index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: [
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: contacts[index].img != null
+                          ? FileImage(File(contacts[index].img))
+                          : AssetImage("images/person.png")),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      contacts[index].nome ?? "",
+                      style: TextStyle(
+                          fontSize: 22.0, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      contacts[index].sobrenome ?? "",
+                      style: TextStyle(fontSize: 18.0)
+                    ),
+                    Text(
+                      contacts[index].telefone ?? "",
+                      style: TextStyle(fontSize: 18.0)
+                    ),
+                    Text(
+                      contacts[index].email ?? "",
+                      style: TextStyle(
+                          fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
