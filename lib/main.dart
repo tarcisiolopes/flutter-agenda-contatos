b0093c54ccf23fc58a3f36a9f903a036709ae1b1
Tarcisio Lopes <tarcisiolopesgomes@gmail.com>
2020-09-08 11:24:16 -0300

    Criando tela de Contato

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:agenda/ui/contact_page.dart';
 import 'package:agenda/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
